3776b52c219e04c917dd0578927d58703ab658ca
Alejandro Betancor <alebeta@gonkar.com>
2023-02-20 18:00:33 +0100

    improvements

---
+++ go.mod
@@ -0,0 +1,16 @@
+module git.gonkar.com/beta/behindflare
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/fatih/color v1.14.1
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
@@ -14,40 +15,44 @@ import (
 	"github.com/fatih/color"
 )
 
-var (
-	protocol      = flag.String("proto", "http", "The protocol used by the site behind CF")
-	domain        = flag.String("domain", "example.com", "Domain target")
-	subnet        = flag.String("subnet", "192.168.0.1/24", "Subnet to scan")
-	OriginalTitle string
-	limit         = flag.Int("jobs", 20, "Number of parallel jobs")
-)
-
 func main() {
+	var protocol, domain, subnet string
+	var limit int
+	var mu sync.Mutex
+	var numScanned int
+	flag.StringVar(&protocol, "proto", "http", "The protocol used by the site behind CF")
+	flag.StringVar(&domain, "domain", "example.com", "Domain target")
+	flag.StringVar(&subnet, "subnet", "192.168.0.1/24", "Subnet to scan")
+	flag.IntVar(&limit, "jobs", 20, "Number of parallel jobs")
 	flag.Parse()
 
 	Banner()
 
-	color.Cyan("Analyzing Domain: %v", *domain)
+	color.Cyan("Analyzing Domain: %v", domain)
 
-	siteInfo()
-
-	ipAddresses2, err := Hosts(*subnet)
+	originalTitle, err := getSiteTitle(protocol, domain)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error reading site title: %v", err)
 	}
 
-	color.Cyan("Number of IPs to scan: %v", len(ipAddresses2))
-	// The `main` func must not finished before all jobs are done. We use a
+	ipAddresses, err := getHosts(subnet)
+	if err != nil {
+		log.Fatalf("Error getting IP addresses: %v", err)
+	}
+
+	color.Cyan("Number of IPs to scan: %v", len(ipAddresses))
+
+	// The `main` func must not finish before all jobs are done. We use a
 	// WaitGroup to wait for all of them.
 	wg := new(sync.WaitGroup)
 
 	// We use a buffered channel as a semaphore to limit the number of
 	// concurrently executing jobs.
-	sem := make(chan struct{}, *limit)
+	sem := make(chan struct{}, limit)
 
 	// We run each job in its own goroutine but use the semaphore to limit
 	// their concurrent execution.
-	for k, i := range ipAddresses2 {
+	for k, i := range ipAddresses {
 		// This job must be waited for.
 		wg.Add(1)
 
@@ -58,97 +63,50 @@ func main() {
 
 		// Now we have acquired the semaphore and can start a goroutine for
 		// this job. Note that we must capture `i` as an argument.
-
 		go func(k int, i string) {
+			defer func() { <-sem }()
+
 			// When the work of this goroutine has been done, we decrement the
 			// WaitGroup.
 			defer wg.Done()
 
-			// When the work of this goroutine has been done, we release the
-			// semaphore.
-			defer func() { <-sem }()
 			// Do the actual work.
-			scanBlock(k, i)
-			//fmt.Printf("IP scanned is %s\n", result)
+			if err := scanHost(k, i, protocol, domain, originalTitle); err != nil {
+				log.Printf("Error scanning host %s: %v", i, err)
+			}
 
+			// Update the counter and print progress.
+			mu.Lock()
+			numScanned++
+			if numScanned%100 == 0 {
+				color.Cyan("Scanned %d hosts", numScanned)
+			}
+			mu.Unlock()
 		}(k, i)
 	}
 
 	// Wait for all jobs to finish.
 	wg.Wait()
-
 }
 
-// Get Body Size
-func siteInfo() {
-
-	resp, err := http.Get("https://" + *domain)
+func getSiteTitle(protocol, domain string) (string, error) {
+	resp, err := http.Get(protocol + "://" + domain)
 	if err != nil {
-		log.Fatal("Error reading request. ", err)
+		return "", err
 	}
-
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Error reading body", err)
+		return "", err
 	}
 
 	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
 
-	OriginalTitle = doc.Find("title").Eq(0).Text()
-
+	return doc.Find("title").Eq(0).Text(), nil
 }
 
-func scanBlock(k int, i string) string {
-
-	req, err := http.NewRequest("GET", *protocol+"://"+i, nil)
-	if err != nil {
-		log.Fatal("Error reading request. ", err)
-	}
-
-	//fmt.Println("Job Number: ", j, "Using IP: ", ips)
-	//fmt.Println("Using IP: ", i)
-
-	req.Host = *domain
-
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		}}
-
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		//fmt.Println("HTTP call failed:", err)
-		return i
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Error reading body", err)
-	}
-
-	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
-
-	title := doc.Find("title").Eq(0).Text()
-
-	switch OriginalTitle {
-	case title:
-		color.Green("##############-HOST FOUND-###################\n")
-		color.Green("Server IP: %v", i)
-		color.Green("HTTP Status: %v", resp.StatusCode)
-		color.Green("#############################################\n")
-		defer resp.Body.Close()
-
-	}
-	return i
-
-}
-
-// Hosts function read a CIDR and create a slide with all the IP addresses contained in it
-func Hosts(cidr string) ([]string, error) {
+func getHosts(cidr string) ([]string, error) {
 	ip, ipnet, err := net.ParseCIDR(cidr)
 	if err != nil {
 		return nil, err
@@ -177,4 +135,46 @@ func inc(ip net.IP) {
 			break
 		}
 	}
+}
+
+func scanHost(k int, ip, protocol, domain, originalTitle string) error {
+	req, err := http.NewRequest("GET", protocol+"://"+ip, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	req.Host = domain
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("HTTP call failed: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading body: %v", err)
+	}
+
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+	title := doc.Find("title").Eq(0).Text()
+
+	switch originalTitle {
+	case title:
+		color.Green("##############-HOST FOUND-###################\n")
+		color.Green("Server IP: %v", ip)
+		color.Green("HTTP Status: %v", resp.StatusCode)
+		color.Green("#############################################\n")
+	}
+
+	return nil
 }
